12fbd36caa8791f5a123744e00dcbf99227d42e6
g0otsug <108853928+g0otsug@users.noreply.github.com>
2024-08-02 10:52:16 +0700

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,5 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 
-cred = credentials.Certificate("/webdigsin-firebase-adminsdk-odsxc-970c5380bc.json")
+cred = credentials.Certificate("webdigsin-firebase-adminsdk-odsxc-970c5380bc.json")
 firebase_admin.initialize_app(cred)
