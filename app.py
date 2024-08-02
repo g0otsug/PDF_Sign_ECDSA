045d355467a422181adc0727a93edc936df24785
g0otsug <108853928+g0otsug@users.noreply.github.com>
2024-08-02 14:57:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def main():
                     st.success(f"Welcome {email}")
                     st.session_state.logged_in = True
                     st.session_state.user_uid = user_uid
-                    st.experimental_set_query_params(logged_in=True)
+                    st.experimental_rerun()  # Refresh the page to go to the main menu
 
     else:
         menu = ["Key Generation", "Sign Document", "Verify Document", "Logout"]
@@ -71,9 +71,8 @@ def main():
 
         if choice == "Logout":
             st.session_state.logged_in = False
-            st.experimental_set_query_params(logged_in=False)
-            st.success("You have been logged out")
-        
+            st.experimental_rerun()  # Refresh the page to go to the login screen
+
         elif choice == "Key Generation":
             st.subheader("Key Generation")
             if st.button("Generate Keys") or ('private_pem' in st.session_state and 'public_pem' in st.session_state):
