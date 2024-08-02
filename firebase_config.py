ec78f62bc643206724b2c4a766d716df0d4f8818
g0otsug <108853928+g0otsug@users.noreply.github.com>
2024-08-02 10:43:04 +0700

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,5 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 
-cred = credentials.Certificate("myecdsasign-firebase-adminsdk-rstgn-81a02c1fc4.json")
+cred = credentials.Certificate("myecdsasign-firebase-adminsdk-rstgn-b824cd1141.json")
 firebase_admin.initialize_app(cred)
