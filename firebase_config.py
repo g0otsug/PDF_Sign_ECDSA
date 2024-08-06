6440384d355c6bae2ab5cf64691ee6978de3bc26
Belajar Kriptografi <108853928+g0otsug@users.noreply.github.com>
2024-08-06 10:26:00 +0700

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,7 @@
 import firebase_admin
-from firebase_admin import credentials, auth, firestore
+from firebase_admin import credentials, auth, db
 
 cred = credentials.Certificate("webdigsin-firebase-adminsdk-odsxc-4464d63e5b.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://webdigsin-default-rtdb.asia-southeast1.firebasedatabase.app/'
+})
