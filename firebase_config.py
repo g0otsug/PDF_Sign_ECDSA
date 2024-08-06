d6607e234718eba88fda760ccb6e8ef3f8d5bdbb
Belajar Kriptografi <108853928+g0otsug@users.noreply.github.com>
2024-08-06 10:56:48 +0700

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,5 +3,5 @@ from firebase_admin import credentials, auth, db
 
 cred = credentials.Certificate("webdigsin-firebase-adminsdk-odsxc-c2b4357f00.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://webdigsin-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    'databaseURL': 'https://ecdsaweb-default-rtdb.firebaseio.com/'
 })
