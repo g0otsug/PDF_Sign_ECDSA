ae1ba83ef6e38da423ea87243d4ca6b22dbd9232
Belajar Kriptografi <108853928+g0otsug@users.noreply.github.com>
2024-08-10 22:09:35 +0700

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, auth, db
 
-cred = credentials.Certificate("ecdsaweb-firebase-adminsdk-zipox-a0c50a087e.json")
+cred = credentials.Certificate("acsproject2024-firebase-adminsdk-f6jqh-823b5dc0a0.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://acsproject2024-default-rtdb.asia-southeast1.firebasedatabase.app/'
 })
