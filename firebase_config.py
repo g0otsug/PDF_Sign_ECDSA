60ff5590d2da465292d3442ab43889d8bde3d5f8
Belajar Kriptografi <108853928+g0otsug@users.noreply.github.com>
2024-08-10 16:47:17 +0700

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, auth, db
 
-cred = credentials.Certificate("ecdsaacs-firebase-adminsdk-lpmmp-6544eeae6b.json")
+cred = credentials.Certificate("ecdsaweb-firebase-adminsdk-zipox-efbd593a67.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://ecdsaweb-default-rtdb.firebaseio.com/'
 })
